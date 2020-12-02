bffbabcdbda4d969269a12e436662956759c6066
Harald Ludwig <23039398+hmludwig@users.noreply.github.com>
2020-12-02 12:08:42 +0100

    improvements/cleanup day02

--- src/day02.py
+++ src/day02.py
@@ -5,17 +5,16 @@ data = f.read().split()
 
 count1 = 0
 count2 = 0
+
 for i in range(0, len(data), 3):
-    minmax = data[i].split('-')
-    minimum = int(minmax[0])
-    maximum = int(minmax[1])
-    search_char = data[i + 1][0]
-    search_string = data[i + 2]
-    if minimum <= search_string.count(
-            search_char) and maximum >= search_string.count(search_char):
+    cminmax = data[i].split('-')
+    cmax = int(cminmax[0])
+    cmin = int(cminmax[1])
+    csearch = data[i + 1][0]
+    password = data[i + 2]
+    if cmin <= password.count(csearch) <= cmax:
         count1 += 1
-    if (search_string[minimum - 1] == search_char) ^ (search_string[maximum - 1]
-                                                      == search_char):
+    if (password[cmin - 1] == csearch) != (password[cmax - 1] == csearch):
         count2 += 1
 
 print(f'Part 1: {count1}')
