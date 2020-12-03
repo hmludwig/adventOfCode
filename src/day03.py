021e96a24bbd345b5f8956d935e58bb7bbf97fe2
Harald Ludwig <23039398+hmludwig@users.noreply.github.com>
2020-12-03 09:53:39 +0100

    Day03

--- src/day03.py
+++ src/day03.py
@@ -2,3 +2,19 @@ import sys
 
 f = open(sys.argv[1])
 data = f.read().split()
+
+
+def slope(data, x, y):
+    cnt = 0
+    j = y
+    for i in range(x, len(data), x):
+        if data[i][j] == '#':
+            cnt += 1
+        j += y
+        j = j % len(data[0])
+    return cnt
+
+
+print(
+    slope(data, 1, 1) * slope(data, 1, 3) * slope(data, 1, 5) *
+    slope(data, 1, 7) * slope(data, 2, 1))
